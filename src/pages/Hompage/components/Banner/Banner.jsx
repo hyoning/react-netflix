b65bf72bffcf50ac6e675bcd260de67e68a8fa8e
김효은 <hyoeun@gmail.com>
2024-04-07 00:53:15 +0900

    배너 추가 작업

--- src/pages/Hompage/components/Banner/Banner.jsx
+++ src/pages/Hompage/components/Banner/Banner.jsx
@@ -1,12 +1,24 @@
+import { Alert } from 'bootstrap';
 import React from 'react'
-import { usePopularMoviesQuery } from "../../../../hook/usePopularMovies";
+import { usePopularMoviesQuery } from '../../../../hook/usePopularMovies';
+import './Banner.style.css'
 
 const Banner = () => {
-    const {data} = usePopularMoviesQuery();
+    const {data, isLoading, isError, error} = usePopularMoviesQuery();
     console.log(data);
+    if(isLoading){
+      <h1>Loading...</h1>
+    }
+    if(isError){
+      <Alert variant='danger'>{error.message}</Alert>
+    }
   return (
-    <div>
-       
+    // eslint-disable-next-line
+    <div className="banner" style={{backgroundImage:"url(" + `https://www.themoviedb.org/t/p/w1066_and_h600_bestv2${data?.results[0].poster_path}` +")"}}>
+       <div className="text-white banner-text-area">
+          <h1>{data?.results[0].title}</h1>
+          <p>{data?.results[0].overview}</p>
+        </div>
     </div>
   )
 }
