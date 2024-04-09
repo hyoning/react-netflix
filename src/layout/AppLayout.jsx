ca905b1b12defda19958fa250c0554df2cd46c09
김효은 <hyoeun@gmail.com>
2024-04-09 15:36:19 +0900

    목록 만들기

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {Outlet} from "react-router-dom"; //자손들을 가져오게 하는 것!
+import {Outlet, useNavigate} from "react-router-dom"; //자손들을 가져오게 하는 것!
 import { ReactComponent as Logo } from "../assets/images/logo.svg";
 import '../layout/AppLayout.style.css';
 
 const AppLayout = () => {
+  const [keyword, setKeyword] = useState("");
+  const navigate = useNavigate()
+  const searchByKeyword = (event) =>{
+    event.preventDefault()
+    // eslint-disable-next-line
+    navigate(`/movies/?q=${keyword}`)
+    setKeyword("");
+  }
   return (
     <div className="">
       <Navbar expand="lg" className="navbar__wrap">
@@ -24,14 +33,16 @@ const AppLayout = () => {
             navbarScroll
           >
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/movies">Link</Nav.Link>
+            <Nav.Link href="/movies">Movies</Nav.Link>
           </Nav>
-          <Form className="d-flex">
+          <Form className="d-flex" onSubmit={searchByKeyword}>
             <Form.Control
               type="search"
               placeholder="Search"
               className="me-2"
               aria-label="Search"
+              value={keyword}
+              onChange={(event) => setKeyword(event.target.value)}
             />
             <Button variant="outline-danger">Search</Button>
           </Form>
--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -1,8 +1,72 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom';
+import { useSearchMovieQuery } from '../../hook/useSearchMovie'
+import { Alert } from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Row, Col} from 'react-bootstrap'
+import MovieCard from '../../common/MovieCard/MovieCard';
+import { useState } from 'react';
+import ReactPaginate from 'react-paginate';
+import './MoviePage.style.css'
+//경로 2가지 
+//nav바에서 클릭해서 온 경우 => popularMovie 보여주기
+//keyword 입력해서 온 경우 => keyword와 관련된 영화들을 보여줌
+
+//page state 만들기
+//페이지네이션 클릭할때마다 page바꾸기
+//page 값이 바뀔때 마다 userSearchMovie page까지 넣어서 fetch
 
 const MoviePage = () => {
+  // eslint-disable-next-line
+  const [query, setQuery] = useSearchParams();
+  const keyword = query.get('q');
+  const [page, setPage] = useState(1);
+
+  const {data, isLoading, isError, error} = useSearchMovieQuery({keyword, page});
+  const handlePageClick = ({selected}) => {
+    setPage(selected + 1)
+
+  }
+    if(isLoading){
+      return <h1>Loading...</h1>
+      }
+    if(isError){
+        return <Alert variant='danger'>{error.message}</Alert>
+    }
   return (
-    <div>MoviePage</div>
+    <div className='movie-page-wrap'>
+        <div>filter</div>
+        <div className='movie-page-list'>
+            <Row>
+              {data.results.map((movie, index) => (
+                <Col key={index} lg={3} xs={6}>
+                  <MovieCard movie={movie}/>
+                </Col>
+              ))}
+            </Row>
+            <ReactPaginate
+                nextLabel=">"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={3}
+                marginPagesDisplayed={2}
+                pageCount={data?.total_pages} // 전체 페이지
+                forcePage={page - 1}
+                previousLabel="<"
+                pageClassName="page-item"
+                pageLinkClassName="page-link"
+                previousClassName="page-item"
+                previousLinkClassName="page-link"
+                nextClassName="page-item"
+                nextLinkClassName="page-link"
+                breakLabel="..."
+                breakClassName="page-item"
+                breakLinkClassName="page-link"
+                containerClassName="pagination"
+                activeClassName="active"
+                renderOnZeroPageCount={null}
+              />
+        </div>
+    </div>
   )
 }
 
